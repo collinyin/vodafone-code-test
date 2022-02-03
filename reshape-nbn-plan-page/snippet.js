897f85cf5bd05893532985e92cfa564b5a8b96e1
Collin <collin_yin@hotmail.com>
2022-02-04 03:38:20 +1100

    Initial reshape NBN Plan page commit

---
+++ reshape-nbn-plan-page/snippet.js
@@ -0,0 +1,14 @@
+/**
+ * Please go to https://www.vodafone.com.au/nbn, and ONLY use your JavaScript snippet to run
+ * in the console in order to make the following changes at once:
+ */
+
+/**
+ * a. Hide hero banner;
+ */
+const heroBanner = document.getElementsByClassName("carousel-container")[0];
+heroBanner.style.display = "none";
+
+/**
+ * b. Re-layout feature list items as per requested on desktop view ONLY;
+ */
